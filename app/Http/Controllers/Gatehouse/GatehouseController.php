5bfdf145fc0dd84ac94860015d0c015abf9305a4
michal.sciubidlo <michal.sciubidlo@limango.pl>
2018-01-13 20:35:03 +0100

    initial commit

---
+++ app/Http/Controllers/Gatehouse/GatehouseController.php
@@ -0,0 +1,17 @@
+<?php
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: michal
+ * Date: 13.01.18
+ * Time: 20:26
+ */
+class GatehouseController extends Controller
+{
+
+    public function index()
+    {
+        echo "asd";
+    }
+}
